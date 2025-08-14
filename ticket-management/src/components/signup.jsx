83bf855ba5a4c95e6e91f0e1ccac58d82ca5ab51
Krishna Patel <138808086+missKrishnaPatel@users.noreply.github.com>
2025-08-15 00:13:49 +0530

    Update signup.jsx

--- ticket-management/src/components/signup.jsx
+++ ticket-management/src/components/signup.jsx
@@ -33,7 +33,7 @@ export default function SignupPage() {
   try {
     // const res = await axios.post(`https://ticket-managementser.onrender.com/api/auth/signup`, formData);
     console.log('!!!!!!!!!!!!!!!!!!!!!! formData =>',formData);
-    const res = await axios.post(`http://localhost:5000/api/auth/signup`, formData);
+    const res = await axios.post(`https://ticket-management-backend-z7nf.onrender.com/api/auth/signup`, formData);
     console.log('Signup Success:', res.data);
     navigate('/'); // Redirect to login
   } catch (err) {
